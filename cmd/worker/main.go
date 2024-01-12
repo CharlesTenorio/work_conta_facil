2eb390bb814c5d3de951e5bb907c80221fb435de
Charles <charlestenorios@gmail.com>
2024-01-12 20:12:09 -0300

    esta lando ok da fila

---
+++ cmd/worker/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"github.com/katana/worker/orcafacil-go/internal/config"
+	"github.com/katana/worker/orcafacil-go/pkg/adapter/mongodb"
+	"github.com/katana/worker/orcafacil-go/pkg/adapter/rabbitmq"
+	"github.com/katana/worker/orcafacil-go/pkg/service"
+)
+
+var (
+	VERSION = "0.1.0-dev"
+	COMMIT  = "ABCDEFG-dev"
+)
+
+func main() {
+
+	filas := []rabbitmq.Fila{
+		{
+			Name:       "QUEUE_PRDS_PARA_COTACAO",
+			Durable:    true,
+			AutoDelete: false,
+			Exclusive:  false,
+		},
+		{
+			Name:       "QUEUE_PRDS_PARA_COTACAO_DLQ",
+			Durable:    true,
+			AutoDelete: false,
+			Exclusive:  false,
+		},
+	}
+
+	conf := config.NewConfig()
+	db_conn := mongodb.New(conf)
+	rbmq_conn := rabbitmq.NewRabbitMQ(filas, conf)
+	task_service := service.NewTaskService(rbmq_conn, db_conn)
+
+	done := make(chan bool)
+	go task_service.Run()
+	log.Printf("Worker Running [Mode: %s], [Version: %s], [Commit: %s]", conf.Mode, VERSION, COMMIT)
+	<-done
+}
